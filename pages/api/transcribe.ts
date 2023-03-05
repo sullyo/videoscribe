86764d9d0f04e81997594450dee20fbbd404aa66
sully <sulaimanmarouf@gmail.com>
2023-03-05 15:53:49 -0800

    fix youtube finally

--- pages/api/transcribe.ts
+++ pages/api/transcribe.ts
@@ -1,3 +1,4 @@
+import { randomUUID } from "crypto";
 import fs from "fs";
 import type { NextApiRequest, NextApiResponse } from "next";
 import FormData from "form-data";
@@ -5,35 +6,49 @@ import fetch from "node-fetch";
 import ytdl from "ytdl-core";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  try {
+  if (req.method === "POST") {
+    const outputFile = `${randomUUID()}.mp4`;
     const { videoUrl, apiKey, format } = req.body;
+    try {
+      const fileStream = ytdl(videoUrl, {
+        quality: "lowest",
+      }).pipe(fs.createWriteStream(outputFile));
 
-    const fileStream = ytdl(videoUrl).pipe(fs.createWriteStream("output.mp4"));
+      await new Promise((resolve) => {
+        fileStream.on("finish", resolve);
+      });
+    } catch (err: any) {
+      console.log(`❌ Error message: ${err.message}`);
+      return res.status(400).send(`Unable to download video: ${err.message}`);
+    }
+    try {
+      const form = new FormData();
+      form.append("file", fs.createReadStream(outputFile));
+      form.append("model", "whisper-1");
+      form.append("response_format", format);
 
-    await new Promise((resolve) => {
-      fileStream.on("finish", resolve);
-    });
-    const form = new FormData();
-    form.append("file", fs.createReadStream("output.mp4"));
-    form.append("model", "whisper-1");
-    form.append("response_format", format);
+      const response = await fetch(
+        "https://api.openai.com/v1/audio/transcriptions",
+        {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${apiKey}`,
+          },
+          body: form,
+        }
+      );
+      const data = await response.text();
 
-    const response = await fetch(
-      "https://api.openai.com/v1/audio/transcriptions",
-      {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${apiKey}`,
-        },
-        body: form,
-      }
-    );
-    const data = await response.text();
-
-    return new Response(JSON.stringify(data), { status: 200 });
-  } catch (error) {
-    console.error(error);
-    return new Response("Error", { status: 500 });
+      res.status(200).json({ transcript: data });
+    } catch (err) {
+      console.log(`❌ Error message: ${err.message}`);
+      return res
+        .status(400)
+        .send(`Issue with transcribing video from openai: ${err.message}`);
+    }
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method Not Allowed");
   }
 };
 
--- pages/youtube.tsx
+++ pages/youtube.tsx
@@ -1,15 +1,16 @@
 import { useState } from "react";
 import Head from "next/head";
 import { apiKeyAtom } from "@/atoms";
+import { toast } from "@/hooks/use-toast";
 import { useAtom } from "jotai";
-import ytdl from "ytdl-core";
 
-import Dropzone from "@/components/dropzone";
+import FileSelect from "@/components/file-select";
 import { Icons } from "@/components/icons";
 import { Layout } from "@/components/layout";
 import { RobotLoader } from "@/components/transcription-loader";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { ToastAction } from "@/components/ui/toast";
 
 export default function YoutubePage() {
   const [loading, setLoading] = useState(false);
@@ -23,32 +24,57 @@ export default function YoutubePage() {
 
   const handleYoutubeDownload = async () => {
     setLoading(true);
-    const videoInfo = await ytdl.getInfo(videoUrl as string);
-    const videoFormat = ytdl.chooseFormat(videoInfo.formats, {
-      quality: "lowest",
-    });
-    const response = await fetch(videoFormat.url);
-    const blob = await response.blob();
-    const file = new File([blob], "output.mp4", { type: "output/mp4" });
-    setFile(file);
-    const formData = new FormData();
-    formData.append("file", file);
-    formData.append("model", "whisper-1");
-    formData.append("response_format", format);
+    try {
+      const response = await fetch("/api/transcribe", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ videoUrl, apiKey, format }),
+      });
 
-    const res = await fetch("https://api.openai.com/v1/audio/transcriptions", {
-      headers: {
-        Authorization: `Bearer ${apiKey}`,
-      },
-      method: "POST",
-      body: formData,
-    });
-
-    const transcript = await res.text();
-
-    console.log(transcript);
-    setLoading(false);
+      const { transcript } = await response.json();
+      const file = new Blob([transcript], { type: "text/plain" });
+      setDownloadFile(file);
+      setDownloadHref(URL.createObjectURL(file));
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      toast({
+        title: "Error Transcribing ",
+        variant: "destructive",
+        description:
+          "OpenAI had an issue transcribing your file. Either try again or try a different file.",
+        action: (
+          <ToastAction
+            altText="Try uploading again"
+            onClick={() => window.location.reload()}
+          >
+            Try again
+          </ToastAction>
+        ),
+      });
+      console.error(error.message);
+    }
   };
+
+  if (apiKey.length < 51) {
+    return (
+      <Layout>
+        <Head>
+          <title>Videoscribe</title>
+          <meta name="description" content="Upload and transcribe your files" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <section className="container grid max-w-4xl items-center gap-6 pt-6 pb-8 md:py-24">
+          <div className="flex flex-col gap-2">
+            <h1 className="text-center text-xl font-extrabold leading-tight tracking-tight sm:text-2xl md:text-3xl lg:text-4xl">
+              Please enter your OpenAI API Key
+            </h1>
+          </div>
+        </section>
+      </Layout>
+    );
+  }
 
   return (
     <Layout>
@@ -59,25 +85,66 @@ export default function YoutubePage() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <section className="container grid max-w-4xl items-center gap-6 pt-6 pb-8 md:py-24">
-        <div className="flex flex-col gap-2">
-          <h1 className="text-center text-xl font-extrabold leading-tight tracking-tight sm:text-2xl md:text-3xl lg:text-4xl">
-            Choose a youtube video & transcribe it
-          </h1>
-        </div>
-        <div className="grid gap-2 md:grid-cols-6">
-          <Input
-            type="text"
-            placeholder="Paste Youtube Link"
-            className="md:col-span-5"
-            onChange={(e) => setVideoUrl(e.target.value)}
-          />
-          <Button
-            className="md:col-span-1"
-            onClick={() => handleYoutubeDownload()}
-          >
-            Start
-          </Button>
-        </div>
+        {loading && (
+          <div className="flex flex-col items-center gap-2">
+            <h1 className="text-center text-2xl font-extrabold leading-tight tracking-tight sm:text-3xl md:text-4xl">
+              Transcribing this youtube video...
+            </h1>
+            <p className="text-lg text-gray-400 dark:text-gray-300 sm:text-xl">
+              This may take a few minutes. Do not close this tab.
+            </p>
+            <RobotLoader />
+          </div>
+        )}
+        {downLoadFile && (
+          <div className="flex flex-col items-center gap-2">
+            <h1 className="text-center text-xl font-extrabold leading-tight tracking-tight sm:text-2xl md:text-3xl lg:text-4xl">
+              Done!
+            </h1>
+            <a
+              href={downloadHref}
+              download={`videoscribe-yt-dl.${
+                format === "text" ? "txt" : format
+              }`}
+              className="flex items-center justify-center text-center text-lg text-slate-700 hover:underline dark:text-slate-400 sm:text-xl"
+            >
+              Download your transcript here
+              <Icons.download className="sm:h-4.5 sm:w-4.5 ml-2 h-4 w-4 text-slate-700 dark:text-slate-400" />
+            </a>
+            <Button
+              className="mx-auto mt-6 w-full sm:w-1/2"
+              onClick={() => window.location.reload()}
+            >
+              Transcribe another video
+            </Button>
+          </div>
+        )}
+        {!loading && !downLoadFile && (
+          <>
+            <div className="flex flex-col gap-2">
+              <h1 className="text-center text-xl font-extrabold leading-tight tracking-tight sm:text-2xl md:text-3xl lg:text-4xl">
+                Choose a youtube video & transcribe it
+              </h1>
+            </div>
+            <div className=" flex flex-col gap-2">
+              <Input
+                type="text"
+                placeholder="Paste Youtube Link"
+                className="md:col-span-3"
+                onChange={(e) => setVideoUrl(e.target.value)}
+              />
+              <div className="flex flex-row justify-center gap-2">
+                <Button
+                  className="w-full"
+                  onClick={() => handleYoutubeDownload()}
+                >
+                  Start
+                </Button>
+                <FileSelect format={format} setFileFormat={setFileFormat} />
+              </div>
+            </div>
+          </>
+        )}
       </section>
     </Layout>
   );
