f258700b4b74fac7525ccfb3a0a7962e2f6d4002
sully <sulaimanmarouf@gmail.com>
2023-03-05 17:27:39 -0800

    update copy

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,26 +21,26 @@ export default function IndexPage() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <section className="container grid max-w-4xl items-center gap-6 pt-6 pb-8 sm:gap-8 md:py-24">
+      <section className="container grid max-w-4xl items-center gap-6 pt-12 pb-8 sm:gap-8 md:py-36">
         <div className="flex flex-col gap-6 sm:gap-8 ">
           <h1 className="text-center text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
             Transcribe any video for free.
           </h1>
-          <p className="mx-auto max-w-xl text-center text-lg text-slate-700 dark:text-slate-400 sm:text-xl">
-            Upload your own file or paste a Youtube video URL. Just add your
-            OpenAi API key and you're good to go.
+          <p className="mx-auto max-w-sm text-center text-lg text-slate-700 dark:text-slate-400 sm:text-xl md:max-w-xl">
+            Turn videos into text in minutes. Whether it's your own files or a
+            YouTube video, we've got you covered.
           </p>
         </div>
 
         <div className="flex justify-center gap-4">
           <Link href={siteConfig.links.video} className={buttonVariants()}>
-            Upload your own file
+            Upload File
           </Link>
           <Link
             href={siteConfig.links.youtube}
             className={buttonVariants({ variant: "outline" })}
           >
-            Youtube Url
+            Youtube Link
           </Link>
         </div>
       </section>
